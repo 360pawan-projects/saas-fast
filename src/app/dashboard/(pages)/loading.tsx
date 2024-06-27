e164718431d83203e3aa479c55fb486333e494d1
360pawan <360pawan@gmail.com>
2024-06-27 10:10:58 +0530

    remove old code - add new architecture for app

--- src/app/dashboard/(pages)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <main className="flex flex-col min-h-screen items-center justify-center text-center">
-      <h1>Loading...</h1>
-    </main>
-  );
-}
